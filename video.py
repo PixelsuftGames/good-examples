2c4badc54def0a1cdaf52f461bca423c34548f3a
Pixelsuft <pixelsuft@gmail.com>
2023-03-07 14:10:38 +0700

    video player

--- video.py
+++ video.py
@@ -13,24 +13,25 @@ class Scene(scene.Scene):
         self.w: main.Window = renderer.window
         self.r: main.Renderer = renderer
         self.w.set_resizable(True)
+        self.size = self.r.get_output_size()
         self.fps_font: gg.TTF = data[0]
         self.music: gg.Music = data[1]
         self.circles = []
         # Not Recommended, but we don't need to check cv2
         self.cap = cv2.VideoCapture(self.a.p(self.a.assets_folder, 'video.mp4'))  # noqa
-        # You can get everything from cap, by I'm lazy)
         self.format = self.r.pixel_format_from_str('bgr24')
-        self.fps = 25
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)  # noqa
+        self.total_frames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)  # noqa
+        self.frames = 0
         self.limit = 1 / self.fps
-        self.size = (960, 720)
-        self.pitch = 3 * self.size[0]
-        self.tex = self.r.create_texture(self.size, self.format)
+        self.video_size = (self.cap.get(cv2.CAP_PROP_FRAME_WIDTH), self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # noqa
+        self.pitch = int(3 * self.video_size[0])
+        self.tex = self.r.create_texture(self.video_size, self.format)
         self.update_frame()
         self.timer = gg.Timer(self.limit, enabled=True, smooth=True)
         self.destroyed = False
         self.a.clock.reset()
         self.music.play()
-        # TODO: progress bar
 
     def update(self, dt: float) -> None:
         self.timer.tick(dt)
@@ -40,6 +41,7 @@ class Scene(scene.Scene):
         if self.destroyed:
             return
         self.r.blit(self.tex)
+        self.r.fill_rect((0, 255, 255, 100), (0, self.size[1] - 10, self.frames / self.total_frames * self.size[0], 10))
         self.r.blit(self.r.texture_from_surface(
             self.fps_font.render_text(f'FPS: {self.a.clock.get_fps()}', (0, 255, 255), blend=True)
         ), dst_rect=(0, self.fps_font.descent))
@@ -53,10 +55,14 @@ class Scene(scene.Scene):
                 self.r.load_scene(main.Scene)
             return
         self.tex.update(frame.ctypes.data, self.pitch)
+        self.frames += 1
 
     def on_key_down(self, event: gg.KeyboardEvent) -> None:
         if event.sym in ('AC Back', 'Escape'):
             self.r.load_scene(main.Scene)
+
+    def on_resize(self, event: gg.WindowEvent) -> None:
+        self.size = self.r.get_output_size()
 
     @staticmethod
     def get_resources() -> tuple:
